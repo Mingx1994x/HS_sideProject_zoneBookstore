e6e8638f0f7fde1032b470f96a7b443414365730
Mingx1994x <mggxmingx@gmail.com>
2024-10-12 15:47:45 +0800

    update : 新增書庫頁面輪播區塊,調整swiper樣式與js

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,6 +1,9 @@
 import Swiper from "swiper";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+
 import 'swiper/css';
-import { Navigation } from "swiper/modules";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 // init Swiper:
 const centeredSwiper = new Swiper(".centeredSwiper", {
@@ -19,6 +22,42 @@ const centeredSwiper = new Swiper(".centeredSwiper", {
         },
         1200: {
             slidesPerView: 1.85,
+        }
+    }
+});
+
+// free-mode
+const freeModeSwiper = new Swiper(".freeMode-swiper-1", {
+    modules: [Pagination],
+    slidesPerView: 1.8,
+    spaceBetween: 12,
+    freeMode: true,
+    loop: true,
+})
+
+// cardSwiper
+const cardSwiper = new Swiper(".cardSwiper-1", {
+    modules: [Pagination, Autoplay],
+    slidesPerView: 1,
+    spaceBetween: 12,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    pagination: {
+        el: ".cardSwiper-pagination",
+        clickable: true
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+        },
+        992: {
+            slidesPerView: 4,
+        },
+        1400: {
+            slidesPerView: 5,
         }
     }
 });
