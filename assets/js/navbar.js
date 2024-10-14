93eca8a623352bd41d53b8d0d7aabcb7e33334d9
Mingx1994x <mggxmingx@gmail.com>
2024-10-14 23:17:12 +0800

    style: 嘗試調整banner背景亮度..與navbar滑動過渡效果

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,10 +1,28 @@
 const navbar = document.querySelector('#scrollFixNavbar');
-console.log(navbar);
+// console.log(navbar);
+// navbar.style.transition = 'background 1s ease';
+// window.addEventListener("scroll", () => {
+//     if (window.scrollY > 40 && window.scrollY < window.innerHeight) {
+//         // console.log(window.scrollY);
+//         navbar.style.background = 'linear-gradient(104.36deg, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.2))';
+//         navbar.style.transition = 'background 1s ease';
+//         navbar.classList.remove("text-bg-transition-primary-100");
+
+
+//     } else if (window.scrollY >= window.innerHeight) {
+//         // console.log(window.scrollY);
+//         navbar.style.background = 'rgba(0, 0, 0,0)';
+//         navbar.classList.add("text-bg-transition-primary-100");
+//     } else {
+//         navbar.classList.remove("text-bg-transition-primary-100");
+//         navbar.style.background = 'rgba(0, 0, 0,0)';
+//     }
+// })
+
 window.addEventListener("scroll", () => {
     if (window.scrollY >= window.innerHeight) {
-        // console.log(window.scrollY);
-        navbar.classList.add("text-bg-transition-primary-100")
+        navbar.classList.add("text-bg-transition-primary-100");
     } else {
-        navbar.classList.remove("text-bg-transition-primary-100")
+        navbar.classList.remove("text-bg-transition-primary-100");
     }
 })
